7875ea8767d34dbb7115b2f1496ba7dadb9bff70
LAPTOP-RQB3NDNV\BALAKRISHNAN <krish.18.2009@gmail.com>
2022-02-01 21:01:30 +0100

    Header implementation

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand">TV Show</a>
+            <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
